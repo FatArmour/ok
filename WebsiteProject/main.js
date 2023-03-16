13b38235892018e11d6ab75ba2a2568010b1a7a2
FatArmour <124217566+FatArmour@users.noreply.github.com>
2023-03-16 12:18:54 -0600

    Add files via upload

---
+++ WebsiteProject/main.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click", btnClicked1);
+document.getElementById("btn").addEventListener("click", btnClicked2);
+document.getElementById("btn").addEventListener("click", btnClicked3);
+document.getElementById("btn").addEventListener("click", btnClicked4);
+function btnClicked1() {}
+function btnClicked2() {}
+function btnClicked3() {}
+function btnClicked4() {}
